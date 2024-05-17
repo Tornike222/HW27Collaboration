02bca6852d03bd7b38537bca765a0fc3f93fed84
mariamsreseli <Sreseli.Mariam@tbcacademy.edu.ge>
2024-05-17 23:34:24 +0400

    added background

--- NatureCollaboration/Scenes/SolarResourcePage/SolarResourceViewController.swift
+++ NatureCollaboration/Scenes/SolarResourcePage/SolarResourceViewController.swift
@@ -10,8 +10,20 @@ import UIKit
 class SolarResourceViewController: UIViewController {
     //MARK: - Properties
     var viewModel: SolarResourceViewModel!
-
     
+    let backgroundImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "BackgroundImage")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    let addressLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     
     
     
@@ -19,11 +31,10 @@ class SolarResourceViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .cyan
+        addBackground()
     }
     
-    
-    
-    
+
     //MARK: - Initialization VM
     init(viewModel: SolarResourceViewModel){
         self.viewModel = viewModel
@@ -33,5 +44,18 @@ class SolarResourceViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    func addBackground() {
+        view.addSubview(backgroundImageView)
+        NSLayoutConstraint.activate([
+        backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+        backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+        backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+        backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        ])
+    }
+}
 
+#Preview {
+    SolarResourceViewController(viewModel: SolarResourceViewModel())
 }
